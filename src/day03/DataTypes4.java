3053921e06091fd9a571d69f399f5ba0c88f4536
ziorye <ziorye@gmail.com>
2023-05-25 21:57:28 +0800

    Add DataTypes4

---
+++ src/day03/DataTypes4.java
@@ -0,0 +1,29 @@
+package day03;
+
+/**
+ * 练习题
+ *
+ * Next
+ * @see StringBasic
+ */
+public class DataTypes4 {
+    public static void main(String[] args) {
+        // 不写 L/l
+        long long1 = 123213;
+        System.out.println(long1);
+        //long long2 = 28889966337788988;
+        long long2 = 28889966337788988L;
+
+
+        // 不写 F/f
+        //float float1 = 12.3;
+        //float float1 = 12.3F;
+        //float float1 = (float)12.3;
+
+        //整型，默认类型 int
+        //浮点型，默认类型 double
+        byte byte1 = 12;
+        //byte byte2 = byte1 + 1;
+        //float float1 = byte1 + 12.3;
+    }
+}
