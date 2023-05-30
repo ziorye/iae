2b28c34cf958057bafcdf67d8dffaff7224b9206
ziorye <ziorye@gmail.com>
2023-05-30 13:14:21 +0800

    Add For4

---
+++ src/day0530/For4.java
@@ -0,0 +1,13 @@
+package day0530;
+
+public class For4 {
+    public static void main(String[] args) {
+        // for ;;
+        /*
+        for (;;) {
+            System.out.println("Hello For.");
+        }
+         */
+
+    }
+}
