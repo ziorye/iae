429c8872af774769f4a0b9ece89263cef89743ce
ziorye <ziorye@gmail.com>
2023-05-30 08:58:52 +0800

    Add OperatorTest

---
+++ src/day0530/OperatorTest.java
@@ -0,0 +1,16 @@
+package day0530;
+
+public class OperatorTest {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+        short z = 40;
+        if ((z++ == 40) && (y = true)) {
+            z++;
+        }
+        if ((x = false) || (++z == 43)) {
+            z++;
+        }
+        System.out.println("z = " + z);
+    }
+}
