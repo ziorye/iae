30e74ec730df33290f5e6b66b2f5247a65fbdeeb
ziorye <ziorye@gmail.com>
2023-05-30 09:02:16 +0800

    Add For

---
+++ src/day0530/For.java
@@ -0,0 +1,34 @@
+package day0530;
+
+/**
+ * for loop
+ *
+ * ① 初始化条件
+ * ② 循环条件  ---> boolean
+ * ③ 循环体
+ * ④ 迭代条件
+ *
+ * https://docs.oracle.com/javase/tutorial/java/nutsandbolts/for.html
+ *
+ * for(①; ②; ④){
+ * 	③
+ * }
+ *
+ * 执行过程：① - ② - ③ - ④ - ② - ③ - ④ - ... - ②不满足 - Exit
+ */
+public class For {
+    public static void main(String[] args) {
+        // 5 times
+        System.out.println("Hello World!");
+
+        /*
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World!");
+        }
+         */
+
+        /*
+        fori
+         */
+    }
+}
