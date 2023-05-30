dc56401ef1714cabb5c527cea266d51609bd513c
ziorye <ziorye@gmail.com>
2023-05-30 09:01:04 +0800

    Add If2

---
+++ src/day0530/If2.java
@@ -0,0 +1,26 @@
+package day0530;
+
+/**
+ * 狗龄 vs 人龄
+ * 换算规则：狗的前两年，每一年相当于人类的10.5岁；
+ *         之后每增加一年就增加四岁。
+ * 例问：5岁的狗相当于人类多少年龄?
+ * 例答：10.5 + 10.5 + 4 + 4 + 4 = 33岁
+ * 
+ * 编写一个程序，获取用户输入的狗的年龄，通过程序显示其相当于人类的年龄。如果用户输入负数，请显示一个提示信息。
+ */
+public class If2 {
+    public static void main(String[] args) {
+        /*
+        int age = 5;
+        if (age >= 0 && age <= 2) {
+            System.out.println("相当于人的年龄：" + age * 10.5);
+        } else if (age > 2) {
+            System.out.println("相当于人的年龄：" + (2 * 10.5 + (age - 2) * 4));
+        } else {
+            System.out.println("不规范的年龄");
+        }
+         */
+        // todo
+    }
+}
