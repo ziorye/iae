e7fa690a2ddd1e03203d4af7af7d7e19637fe5da
ziorye <ziorye@gmail.com>
2023-05-30 13:53:33 +0800

    Add Table99

---
+++ src/day0530/Table99.java
@@ -0,0 +1,22 @@
+package day0530;
+
+/**
+ * 九九乘法表
+ * 1 * 1 = 1
+ * 2 * 1 = 2  2 * 2 = 4
+ * ...
+ * 9 * 1 = 9 ... 9 * 9 = 81
+ */
+public class Table99 {
+    public static void main(String[] args) {
+        /*
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " * " + j + " = " + (i * j) + "  ");
+            }
+            System.out.println();
+        }
+         */
+        // coding...
+    }
+}
