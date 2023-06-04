4775bcd30fcc559e0a9956d0e55391414becd975
ziorye <ziorye@gmail.com>
2023-06-04 20:33:18 +0800

    TwoDTest2

---
+++ src/day0605/TwoDTest2.java
@@ -0,0 +1,25 @@
+package day0605;
+
+/**
+ * 例
+ * 问题描述
+ * int[] x, y[]; 在给x,y变量赋值以后，以下选项允许通过编译的是：
+ *
+ * a)    x[0] = y;
+ * b)    y[0] = x;
+ * c)    y[0][0] = x;
+ * d)    x[0][0] = y;
+ * e)    y[0][0] = x[0];
+ * f)    x = y;
+ */
+public class TwoDTest2 {
+    public static void main(String[] args) {
+        int[] x, y[];
+//        x[0] = y;
+//        y[0] = x;
+//        y[0][0] = x;
+//        x[0][0] = y;
+//        y[0][0] = x[0];
+//        x = y;
+    }
+}
