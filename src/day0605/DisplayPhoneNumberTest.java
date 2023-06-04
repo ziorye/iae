e28df085ba3c43c1a593b68968388858e7c90fe7
ziorye <ziorye@gmail.com>
2023-06-04 20:33:18 +0800

    DisplayPhoneNumberTest

---
+++ src/day0605/DisplayPhoneNumberTest.java
@@ -0,0 +1,24 @@
+package day0605;
+
+/**
+ * 问题描述：
+ *   已知 array1 和 array2
+ *   求电话号码
+ *
+ */
+public class DisplayPhoneNumberTest {
+    public static void main(String[] args) {
+        int[] array1 = new int[] { 8, 2, 1, 0, 3 };
+        int[] array2 = new int[] { 2, 0, 3, 2, 4, 0, 1, 3, 2, 3, 3 };
+
+        /*
+        String phoneNumber = "";
+        for (int i = 0; i < array2.length; i++) {
+            int value = array2[i];
+            phoneNumber += array1[value];
+        }
+        System.out.println("phoneNumber：" + phoneNumber);
+         */
+
+    }
+}
