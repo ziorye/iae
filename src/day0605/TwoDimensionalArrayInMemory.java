928850dff7f921b388e1043feaae5b2805d1675f
ziorye <ziorye@gmail.com>
2023-06-04 20:33:18 +0800

    TwoDimensionalArrayInMemory

---
+++ src/day0605/TwoDimensionalArrayInMemory.java
@@ -0,0 +1,11 @@
+package day0605;
+
+public class TwoDimensionalArrayInMemory {
+    public static void main(String[] args) {
+        String[][] arr1 = new String[3][2];
+        int[][] arr2 = new int[4][];
+        arr2[1] = new int[5];
+        arr2[1][1] = 1;
+        arr2[2][2] = 1;
+    }
+}
