3323b44796a86f06ce1223fa2c0086951a3463de
ziorye <ziorye@gmail.com>
2023-06-04 20:33:18 +0800

    TwoDTest1

---
+++ src/day0605/TwoDTest1.java
@@ -0,0 +1,29 @@
+package day0605;
+
+/**
+ * 求和
+ * 见图 md/img_4.png
+ */
+public class TwoDTest1 {
+    public static void main(String[] args) {
+        // 1. 如何表示这些数据
+        /*
+        二维数组
+        静态初始化
+        int[][] arr = new int[][]{{3, 5, 8}, {12, 9}, {7, 0, 6, 4}};
+
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+
+        System.out.println("总和为：" + sum);
+         */
+        //int[][] arr = ...;
+
+        // 2. 遍历 + 求和
+
+    }
+}
