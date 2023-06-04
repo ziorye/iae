258a352eda08e3ab428aa669044db4beb8e79ccd
ziorye <ziorye@gmail.com>
2023-06-04 20:33:18 +0800

    DisplayWeekTest

---
+++ src/day0605/DisplayWeekTest.java
@@ -0,0 +1,57 @@
+package day0605;
+
+import java.util.Scanner;
+
+/**
+ * 问题描述
+ *   用一个数组保存星期一到星期天的7个英文单词
+ *   从键盘输入1-7，显示对应星期几的英文单词
+ *
+ * 预期输出
+ *   1 Monday
+ *   2 Tuesday
+ *   3 Wednesday
+ *   4 Thursday
+ *   5 Friday
+ *   6 Saturday
+ *   7 Sunday
+ *
+ * === === ===
+ *
+ * 拓展练习：一年12个月的例子
+ *   用一个数组保存12个月的英文单词，从键盘输入1-12，显示对应月份的英文单词
+ *
+ * 预期输出
+ *   1 January
+ *   2 February
+ *   3 March
+ *   4 April
+ *   5 May
+ *   6 June
+ *   7 July
+ *   8 August
+ *   9 September
+ *   10 October
+ *   11 November
+ *   12 December
+ *
+ */
+public class DisplayWeekTest {
+    public static void main(String[] args) {
+        /*
+        String[] weeks = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入数值（1-7）：");
+        int day = scanner.nextInt();
+
+        if(day < 1 || day > 7){
+            System.out.println("你输入的数据有误，请重新输入");
+        }else{
+            System.out.println(weeks[day - 1]);
+        }
+
+        scanner.close();
+         */
+    }
+}
