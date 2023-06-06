b81b0debfc20462d9d2cda25448d89464e3dcf09
ziorye <ziorye@gmail.com>
2023-06-06 17:28:18 +0800

    Member

---
+++ src/day0607/Member.java
@@ -0,0 +1,25 @@
+package day0607;
+
+/**
+ * Object-Oriented Programming
+ * OOP
+ * 面向对象编程
+ *
+ * An object is a software bundle of related state and behavior.
+ *
+ * A class is a blueprint or prototype from which objects are created.
+ *
+ * In OOP, we create objects to solve complex problems.
+ *
+ * state and behavior
+ *   - An object stores its state in fields (variables in some programming languages)
+ *   - and exposes its behavior through methods (functions in some programming languages)
+ *
+ * You may also occasionally see the term "member" used as well. A type's fields, methods, and nested types are collectively called its members.
+ *
+ */
+public class Member {
+    // state
+
+    // behavior
+}
