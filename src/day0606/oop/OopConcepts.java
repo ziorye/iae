a140cce3e60425437446adb3620c1eef920f4814
ziorye <ziorye@gmail.com>
2023-06-06 17:26:56 +0800

    OopConcepts

---
+++ src/day0606/oop/OopConcepts.java
@@ -0,0 +1,45 @@
+package day0606.oop;
+
+/**
+ * Object-Oriented Programming
+ * OOP
+ * 面向对象编程 VS 面向过程编程
+ *
+ * An object is a software bundle of related state and behavior.
+ *
+ * A class is a blueprint or prototype from which objects are created.
+ *
+ */
+public class OopConcepts {
+    public static void main(String[] args) {
+        String link1 = "https://docs.oracle.com/javase/tutorial/java/concepts/index.html";
+        String link2 = "https://docs.oracle.com/javase/tutorial/java/concepts/object.html";
+        String link3 = "https://docs.oracle.com/javase/tutorial/java/nutsandbolts/variables.html";
+        System.out.println(link1);
+        System.out.println(link2);
+        System.out.println(link3);
+
+        /*
+         * Q:
+         * 理解OOP
+         * 面向对象编程 ___
+         *
+         * A.
+         * 是一个新的编程语言
+         *
+         * B.
+         * 是方法的另一种叫法
+         *
+         * C.
+         * 不涉及创建对象和类
+         *
+         * D.
+         * 是一种通过创建对象来解决问题的技术
+         *
+         */
+
+        /*
+        In OOP, we create objects to solve complex problems.
+         */
+    }
+}
