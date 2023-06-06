8144e6d67b14355594f7be6f969ac15ee48b5f4e
ziorye <ziorye@gmail.com>
2023-06-06 17:28:18 +0800

    Exercises Dog

---
+++ src/day0607/exercises/Dog.java
@@ -0,0 +1,15 @@
+package day0607.exercises;
+
+/**
+ * Exercises 2
+ *
+ * 面向对象思维练习
+ *
+ * 写一个狗Dog类。包含三个state：编号id、名称name、颜色color
+ * 写一个狗Dog测试类。实例化(new)出两只狗，为其state赋值，同时输出两只狗信息
+ *
+ * 预期输出
+ *   狗(**编号)的信息：编号=**、名称=**、颜色=**
+ */
+public class Dog {
+}
