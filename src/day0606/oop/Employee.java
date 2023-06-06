9c6d04454cc697e5b57c71a72470a1982f95f568
ziorye <ziorye@gmail.com>
2023-06-06 17:26:56 +0800

    Employee

---
+++ src/day0606/oop/Employee.java
@@ -0,0 +1,12 @@
+package day0606.oop;
+
+/**
+ * 练习
+ * 员工类 Employee，包含属性：编号id、姓名name、年龄age、薪资salary
+ * EmployeeTest 测试类，并在 main 方法中，创建2个员工对象，为属性赋值，并打印两个员工的信息
+ *
+ * 预期输出
+ * 员工**的信息：编号=**、姓名=**、年龄=**、薪资=**
+ */
+public class Employee {
+}
