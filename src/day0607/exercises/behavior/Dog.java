1ab12ba2670d082947f5069cbed9e6f4416ef450
ziorye <ziorye@gmail.com>
2023-06-06 17:28:18 +0800

    behavior exercises

---
+++ src/day0607/exercises/behavior/Dog.java
@@ -0,0 +1,19 @@
+package day0607.exercises.behavior;
+
+/**
+ * Exercises
+ *
+ * 面向对象思维练习
+ *
+ * 写一个狗Dog类
+ *   - 包含三个state：编号id、名称name、颜色color
+ *   - 包含3个behavior：吠叫barking、摇尾巴wagging tail
+ * 写一个狗Dog测试类
+ *   - 实例化(new)出两只狗，为其state赋值，同时输出两只狗信息
+ *   - 并调用他们的 behavior
+ *
+ * 预期输出
+ *   狗(**编号)的信息：编号=**、名称=**、颜色=**
+ */
+public class Dog {
+}
---
+++ src/day0607/exercises/behavior/Student.java
@@ -0,0 +1,20 @@
+package day0607.exercises.behavior;
+
+/**
+ * Exercises
+ *
+ * 面向对象思维练习
+ *
+ * 写一个学生类
+ *   - 包含3个state：学号id、姓名name、年龄age
+ *   - 包含3个behavior：吃饭eat、睡觉sleep、打游戏play
+ *
+ * 写一个学生测试类
+ *   - 实例化(new)出两个学生，为其state赋值，同时输出两个学生信息
+ *   - 并调用他们的 behavior
+ *
+ * 预期输出
+ *   学生(**学号)的信息：学号=**、姓名=**、年龄=**
+ */
+public class Student {
+}
