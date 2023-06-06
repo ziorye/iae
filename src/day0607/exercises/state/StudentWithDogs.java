7b2a2731cefa92c05564ec8e5b9a65f1608637c0
ziorye <ziorye@gmail.com>
2023-06-06 17:28:18 +0800

    StudentWithDogs

---
+++ src/day0607/exercises/state/StudentWithDogs.java
@@ -0,0 +1,15 @@
+package day0607.exercises.state;
+
+/**
+ * Exercises
+ *
+ * 面向对象思维练习
+ *
+ * 写一个学生类。包含3个state：学号id、姓名name、年龄age、狗狗们dogs
+ * 写一个学生测试类。实例化(new)出两个学生，为其state赋值，同时输出两个学生信息
+ *
+ * 预期输出
+ *   学生(**学号)的信息：学号=**、姓名=**、年龄=**、狗狗们=***
+ */
+public class StudentWithDogs {
+}
