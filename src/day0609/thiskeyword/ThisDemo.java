1bc4b15b85f09de2234db512e1b64a25e06311c1
ziorye <ziorye@gmail.com>
2023-06-08 22:55:54 +0800

    this

---
+++ src/day0609/thiskeyword/ThisDemo.java
@@ -0,0 +1,9 @@
+package day0609.thiskeyword;
+
+public class ThisDemo {
+    public static void main(String[] args) {
+        // class Person
+        // state behavior
+        // constructor this()
+    }
+}
