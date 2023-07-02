37857ef6973093d1c3c56a6791efb4aab47902a7
ziorye <ziorye@gmail.com>
2023-07-02 16:07:00 +0800

    override test2

---
+++ src/day0703/b_override/test2/OrderTest.java
@@ -0,0 +1,19 @@
+package day0703.b_override.test2;
+
+import day0703.b_override.test1.Order;
+
+public class OrderTest {
+    public static void main(String[] args) {
+        Order order = new Order();
+
+//        order.orderPublic = 1;
+//        order.orderDefault = 1;
+//        order.orderProtected = 1;
+//        order.orderPrivate = 1;
+
+//        order.methodPublic();
+//        order.methodDefault();
+//        order.methodProtected();
+//        order.methodPrivate();
+    }
+}
---
+++ src/day0703/b_override/test2/SubOrder.java
@@ -0,0 +1,17 @@
+package day0703.b_override.test2;
+
+import day0703.b_override.test1.Order;
+
+public class SubOrder extends Order {
+    void method() {
+//        orderPublic = 1;
+//        orderDefault = 1;
+//        orderProtected = 1;
+//        orderPrivate = 1;
+
+//        methodPublic();
+//        methodDefault();
+//        methodProtected();
+//        methodPrivate();
+    }
+}
