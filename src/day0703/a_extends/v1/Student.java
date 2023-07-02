9b626b1cba11759c5e8c141aaeb90df171a9d112
ziorye <ziorye@gmail.com>
2023-07-02 11:10:17 +0800

    extends v1

---
+++ src/day0703/a_extends/v1/Student.java
@@ -0,0 +1,20 @@
+package day0703.a_extends.v1;
+
+public class Student {
+    String name;
+    int age;
+
+    String school;
+
+    void eat() {
+        System.out.println("Student.eat");
+    }
+
+    void play() {
+        System.out.println("Student.play");
+    }
+
+    void study() {
+        System.out.println("Student.study");
+    }
+}
---
+++ src/day0703/a_extends/v1/Test.java
@@ -0,0 +1,13 @@
+package day0703.a_extends.v1;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.eat();
+        person.play();
+
+        Student student = new Student();
+        student.eat();
+        student.play();
+    }
+}
