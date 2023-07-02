130155f7b04be8e6c6cb569b989d69ea54e94956
ziorye <ziorye@gmail.com>
2023-07-02 19:05:59 +0800

    polymorphism interview

---
+++ src/day0703/d_polymorphism/interview/FieldMethodTest.java
@@ -0,0 +1,33 @@
+package day0703.d_polymorphism.interview;
+
+class Base {
+    int count = 10;
+
+    public void display() {
+        System.out.println(this.count);
+    }
+}
+
+class Sub extends Base {
+    int count = 20;
+
+    public void display() {
+        System.out.println(this.count);
+    }
+}
+
+public class FieldMethodTest {
+    public static void main(String[] args) {
+        Sub s = new Sub();
+        System.out.println(s.count);
+        s.display();
+        Base b = s;
+        System.out.println(b == s);
+        System.out.println(b.count);
+        b.display();
+
+        Base b1 = new Base();
+        System.out.println(b1.count);
+        b1.display();
+    }
+}
