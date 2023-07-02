0ff27529d68bb651f6342accbe98b1d72aceb953
ziorye <ziorye@gmail.com>
2023-07-02 15:53:09 +0800

    override test1

---
+++ src/day0703/b_override/test1/Order.java
@@ -0,0 +1,35 @@
+package day0703.b_override.test1;
+
+public class Order {
+    // state
+    private int orderPrivate;
+    int orderDefault;
+    protected int orderProtected;
+    public int orderPublic;
+
+    // behavior
+    private void methodPrivate(){
+
+    }
+    void methodDefault(){
+
+    }
+    protected void methodProtected() {
+
+    }
+    public void methodPublic(){
+
+    }
+
+    public void test(){
+//        orderPrivate = 1;
+//        orderDefault = 2;
+//        orderProtected = 3;
+//        orderPublic = 4;
+
+//        methodPrivate();
+//        methodDefault();
+//        methodProtected();
+//        methodPublic();
+    }
+}
---
+++ src/day0703/b_override/test1/OrderTest.java
@@ -0,0 +1,20 @@
+package day0703.b_override.test1;
+
+public class OrderTest {
+    public static void main(String[] args) {
+        Order order = new Order();
+
+        //调用属性
+//        order.orderDefault = 1;
+//        order.orderPublic = 2;
+//        order.orderProtected = 3;
+//        order.orderPrivate = 4;
+
+        //调用方法
+//        order.methodDefault();
+//        order.methodPublic();
+//        order.methodProtected();
+//        order.methodPrivate();
+
+    }
+}
