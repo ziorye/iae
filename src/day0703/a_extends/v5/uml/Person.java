ae891a09cf0e16e3af0d8d04e8b4e22aee23a61c
ziorye <ziorye@gmail.com>
2023-07-02 12:38:44 +0800

    extends uml ...
    open with ...

---
+++ src/day0703/a_extends/v5/uml/Person.java
@@ -0,0 +1,14 @@
+package day0703.a_extends.v5.uml;
+
+public class Person {
+    public String name;
+    int age;
+
+    void eat() {
+        System.out.println("Person.eat");
+    }
+
+    void play() {
+        System.out.println("Person.play");
+    }
+}
