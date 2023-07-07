0ffcb8294b2dac86a916606dd536b91eec6840e8
ziorye <ziorye@gmail.com>
2023-07-07 15:30:46 +0800

    slack webhooks demo

---
+++ src/main/java/day0707/slack/WebhooksDemo.java
@@ -0,0 +1,48 @@
+package day0707.slack;
+
+import com.slack.api.Slack;
+import com.slack.api.webhook.Payload;
+import com.slack.api.webhook.WebhookResponse;
+
+import java.io.IOException;
+
+import static com.slack.api.webhook.WebhookPayloads.payload;
+
+public class WebhooksDemo {
+    private static final Slack SLACK = Slack.getInstance();
+    private static final String WEBHOOK_URL = "https://hooks.slack.com/services/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";
+
+    public static void main(String[] args) throws IOException {
+        WebhooksDemo.simpleText();
+        //WebhooksDemo.textWithLink();
+        //WebhooksDemo.lambdaPayload();
+    }
+
+    public static void simpleText() throws IOException {
+        Payload payload = Payload.builder().text("Hello, World!").build();
+
+        WebhookResponse response = SLACK.send(WEBHOOK_URL, payload);
+
+        if (response.getCode() == 200) {
+            System.out.println(response.getBody());
+        }
+    }
+
+    public static void textWithLink() throws IOException {
+        Payload payload = Payload.builder().text("Hello, World! <https://slack.com|slack>").build();
+
+        WebhookResponse response = SLACK.send(WEBHOOK_URL, payload);
+
+        if (response.getCode() == 200) {
+            System.out.println(response.getBody());
+        }
+    }
+
+    public static void lambdaPayload() throws IOException {
+        WebhookResponse response = SLACK.send(WEBHOOK_URL, payload(p -> p.text("Hello, World! lambda payload")));
+
+        if (response.getCode() == 200) {
+            System.out.println(response.getBody());
+        }
+    }
+}
